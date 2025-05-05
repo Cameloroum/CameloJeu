44a57e5236e0147042654189b852a72d26f7b88d
Cameloroum <ghali.elomari@gmail.com>
2025-05-05 15:57:01 +0100

    First commit

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pygame
+import random
 import sys
-#salut
+
+# Initialisation
+pygame.init()
+largeur, hauteur = 800, 600
+fenetre = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Ghali")
+clock = pygame.time.Clock()
+police = pygame.font.SysFont(None, 36)
+
+# Couleurs
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+
+
+# Boucle principale
+continuer = True
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
+
+
+    fenetre.fill(NOIR)
+
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
